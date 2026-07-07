1bca4d3365b43e72759e1eeeffdc4862f829ab59
Adem <ademcran4aliev@abv.bg>
2026-07-07 21:36:23 +0300

    Updates

---
+++ assets/NavigateNextRounded-Dclle92j.js
@@ -0,0 +1,1 @@
+import{n as e,ot as t}from"./Stack-DE7wPN2r.js";var n=e((0,t().jsx)(`path`,{d:`M9.31 6.71c-.39.39-.39 1.02 0 1.41L13.19 12l-3.88 3.88c-.39.39-.39 1.02 0 1.41s1.02.39 1.41 0l4.59-4.59c.39-.39.39-1.02 0-1.41L10.72 6.7c-.38-.38-1.02-.38-1.41.01`}),`NavigateNextRounded`);export{n as t};
---
+++ assets/RemoveRounded-Dtiy2Wou.js
@@ -0,0 +1,1 @@
+import{n as e,ot as t}from"./Stack-DE7wPN2r.js";var n=t(),r=e((0,n.jsx)(`path`,{d:`M18 13h-5v5c0 .55-.45 1-1 1s-1-.45-1-1v-5H6c-.55 0-1-.45-1-1s.45-1 1-1h5V6c0-.55.45-1 1-1s1 .45 1 1v5h5c.55 0 1 .45 1 1s-.45 1-1 1`}),`AddRounded`),i=e((0,n.jsx)(`path`,{d:`M18 13H6c-.55 0-1-.45-1-1s.45-1 1-1h12c.55 0 1 .45 1 1s-.45 1-1 1`}),`RemoveRounded`);export{r as n,i as t};
